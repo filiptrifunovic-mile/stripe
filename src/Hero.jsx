1b1772d572650c7b14c62ca7b9fa9efa0c367e5d
unknown <filip.trifunovic23@gmail.com>
2023-05-12 14:13:08 +0200

    hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,9 +4,26 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
+  const { closeSubmenu } = useGlobalContext();
 
-  return <h2>hero component</h2>;
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Stripe, Inc. is an Irish-American financial services and software as
+            a service company dual-headquartered in South San Francisco,
+            California, United States and Dublin, Ireland
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
