e5d6c343be6d85c51552fe99b2956bfa51a5bdec
unknown <filip.trifunovic23@gmail.com>
2023-05-12 13:49:43 +0200

    setting up context

---
+++ src/context.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useContext, createContext } from "react";
+import sublinks from "./data";
+
+const AppContext = createContext();
+
+export function AppProvider({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  function openSidebar() {
+    setIsSidebarOpen(true);
+  }
+
+  function closeSidebar() {
+    setIsSidebarOpen(false);
+  }
+
+  function openSubmenu() {
+    setIsSubmenuOpen(true);
+  }
+
+  function closeSubmenu() {
+    setIsSubmenuOpen(false);
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+        isSidebarOpen,
+        isSubmenuOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export function useGlobalContext() {
+  return useContext(AppContext);
+}
